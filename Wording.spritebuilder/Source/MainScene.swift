927a80c320b83fa6ad55d362785844e7f88b30ec
sheriffsoco <japenes@gmail.com>
2015-05-06 11:31:54 -0700

    Add button images to spritebuilder

--- Wording.spritebuilder/Source/MainScene.swift
+++ Wording.spritebuilder/Source/MainScene.swift
@@ -2,6 +2,16 @@ import Foundation
 
 class MainScene: CCNode {
     var nextScene: NSString = "Game"
+    var whiteZeroButton : CCButton!
+    var whiteOneButton : CCButton!
+    var redZeroButton : CCButton!
+    var redOneButton : CCButton!
+    var blueZeroButton : CCButton!
+    var blueOneButton : CCButton!
+    var greenZeroButton : CCButton!
+    var greenOneButton : CCButton!
+    var orangeZeroButton : CCButton!
+    var orangeOneButton : CCButtton!
     
     func didLoadFromCCB() {
         self.userInteractionEnabled = true
