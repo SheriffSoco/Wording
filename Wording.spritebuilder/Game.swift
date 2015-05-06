ae9799d9a145c51619466f055d3ac76db24abf62
sheriffsoco <japenes@gmail.com>
2015-05-06 10:44:56 -0700

    Add spritebuilder project and rtf

---
+++ Wording.spritebuilder/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Wording
+//
+//  Created by Jacob Apenes on 5/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Game: CCScene {
+    
+}
---
+++ Wording.spritebuilder/Source/MainScene.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class MainScene: CCNode {
+    var nextScene: NSString = "Game"
+    
+    func didLoadFromCCB() {
+        self.userInteractionEnabled = true
+    }
+    
+    override func update(delta: CCTime) {
+        if nextScene == "Game" {
+            let mainScene: CCScene = CCBReader.loadAsScene(nextScene)
+            CCDirector.sharedDirector().replaceScene(mainScene);
+        }
+    }
+}
